ef938df13ac1e1b8063ce6f95b0da95b9c9a859d
Jessica P Sousa <jessica.persou@gmail.com>
2022-05-23 14:19:07 -0300

    Working with class and attribute

---
+++ ClassAndAttributes/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        People p1 = new People();
+        p1.name = "Luiz";
+        p1.age = 17;
+
+        System.out.println(p1.name);
+    }
+}
