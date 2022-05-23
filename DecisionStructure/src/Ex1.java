a89c67fad8dc72ad7aeb1c9021691fc0115ffc58
Jessica P Sousa <jessica.persou@gmail.com>
2022-05-23 10:08:45 -0300

    Decisions Structure

---
+++ DecisionStructure/src/Ex1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex1 {
+        /* Faça um programa que receba três inteiros e diga qual é o maior:*/
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Insert first number: ");
+            int n1 = sc.nextInt();
+
+    }
+
+}
