e6753a1c18f2e544751a8cf66febdb986f88f3ac
Jessica P Sousa <jessica.persou@gmail.com>
2022-05-23 10:17:56 -0300

    Exercice one

--- DecisionStructure/src/Ex1.java
+++ DecisionStructure/src/Ex1.java
@@ -6,7 +6,18 @@ public class Ex1 {
             Scanner sc = new Scanner(System.in);
             System.out.println("Insert first number: ");
             int n1 = sc.nextInt();
+            System.out.println("Insert second number: ");
+            int n2 = sc.nextInt();
+            System.out.println("Insert third number: ");
+            int n3 = sc.nextInt();
 
+            if(n1>n2 && n1>n3){
+                System.out.println("First number is bigger that anothers!");
+            }else if(n2>n1 && n2>n3){
+                System.out.println("Second number is bigger that anothers!");
+            }else{
+                System.out.println("Third number is bigger that anothers!");
+            }
     }
 
 }
