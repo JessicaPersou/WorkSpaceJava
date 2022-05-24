43a7f62528f27c92c7256f16de636a17cdd52330
Jessica P Sousa <jessica.persou@gmail.com>
2022-05-23 22:37:27 -0300

    Polymorphism

---
+++ ClassAndAttributes/src/Polymorphism/Product.java
@@ -0,0 +1,40 @@
+package Polymorphism;
+
+public class Product {
+    private String name;
+    private String description;
+    private int amount;
+    private double value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
