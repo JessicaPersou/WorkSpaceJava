6cb564a4d3653cd3ccce1876bc35ee516d8a83ef
JessicaPersou <jessica.sousa@koin.com.br>
2022-08-22 14:41:00 -0300

    Desafio Java

--- PracticeOOP/src/Main.java
+++ PracticeOOP/src/Main.java
@@ -3,20 +3,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        FolhaPagamento joao = new FolhaPagamento();
+        joao.horasNormais = 160;
+        joao.QtdHorasNormaisTrabalhadas = 50.75;
+        joao.horasExtras = 58;
+        joao.QtdHorasExtrasTrabalhadas = 75.25;
+
+        //System.out.println(joao.calcularSalario());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //MÉTODO DA CLASSE DESCONTO EMPREGRADO E PESSOA
+
+        /*Scanner sc = new Scanner(System.in);
         DescontosDoEmpregado dev = new DescontosDoEmpregado();
 
-        /*System.out.println("Informe o seu primeiro nome: ");
+        System.out.println("Informe o seu primeiro nome: ");
         dev.nome = sc.next();
         System.out.println("Olá, " + dev.nome + " me informe o salário bruto por favor: "  );
         dev.salario = sc.nextDouble();
-        System.out.println("O seu salário líquido é: " + dev.descontoINSS());*/
+        System.out.println("O seu salário líquido é: " + dev.descontoINSS());
 
         Pessoa f1 = new Pessoa();
         f1.nome = "Jõao";
-        f1.salarioLiquido = 4050.00;
+        f1.salarioLiquido = 7050.00;
 
-        System.out.println("o seu salario e R$: " +  dev.descontoINSS(f1));
+        System.out.println("o seu salario e R$: " +  dev.descontoINSS(f1));*/
     }
 
 }
