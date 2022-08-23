f417ffab0b90982602c654a3afe8e01792eabfdf
JessicaPersou <jessica.sousa@koin.com.br>
2022-08-23 18:01:13 -0300

    Adicionando calculo IMC

--- OrientingAnObject/Imc.java
+++ OrientingAnObject/Imc.java
@@ -1,6 +1,17 @@
+import java.util.Date;
+
 public class Imc {
 
-    public String weight;
-    public String height;
+    public double weight;
+    public double height;
+
+    People pacient;
+
+    void pacientActive(){
+        System.out.println("The pacient is active");
+    }
+    double calcImc(){
+        return (weight*weight)/height;
+    }
 
 }
--- OrientingAnObject/Main.java
+++ OrientingAnObject/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Imc exameJessica = new Imc();
+        exameJessica.pacient = new People();
+        exameJessica.height= 1.65;
+        exameJessica.weight = 67.8;
 
+
+        System.out.println(exameJessica.calcImc());
     }
 }
